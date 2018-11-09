7dfb72c083ff0f42185aa9d6a9fb8c85ae88472e
Miniminis <minhee4735@gmail.com>
2018-11-09 21:10:48 +0300

    Add and modified 01-conversation.py and 02-data-type-1.py

--- 01-conversation.py
+++ 01-conversation.py
@@ -1,9 +1,11 @@
 # 화면에 데이터 출력하기
-# print(1 + 2)
+print(1 + 2)
+
 # 사용자로부터 값 받기
-# 변수 활용
-# name = input("당신의 이름은 무엇입니까?")
-# print("안녕하세요, " + name)
-# print("hello" + "pretty")
-person = input("내가 바보처럼 보여?")
-print("똥구멍 같은" + person)
+#변수 활용(name)
+name = input("당신의 이름은 무엇입니까?")
+print("안녕하세요," + name)
+menu = input("어떤 메뉴를 먹었어요?")
+print( menu + "은 너무 질리지 않니?")
+
+print("안녕"+"하세요")
---
+++ 02-data-type-1.py
@@ -0,0 +1,23 @@
+#정수 - integer
+print(1+3)
+#소수 float
+print(1.5+2.7)
+#문자 str-string
+print("hello")
+
+#문자열 더하기
+print("hello"+"1")
+
+#정수 소수 간 변경
+print(4//2)
+print(int(4/2))
+print(float(2))
+
+#문자열 숫자 간 변경
+print("1")
+print(int("1"))
+
+#그렇다면 1.5를 정수로 바꿔달라고 요청하면 결과는?
+print(int("1.5"))
+# 예상결과 1) 1 왜냐하면, print(int(1.5))
+# 예상 결과 2) error
